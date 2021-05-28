88ffa23822610850d64076a5cc2cc1ca99cc0954
Derk Gortemaker <derk@gortemaker.nl>
2021-05-28 14:44:18 +0200

    option to not set prio in centos7

--- manifests/repo/centos7.pp
+++ manifests/repo/centos7.pp
@@ -15,6 +15,7 @@
 #
 class yum::repo::centos7 (
   $mirror_url = undef,
+  $setprio = true,
 ) {
 
   if $mirror_url {
@@ -45,49 +46,59 @@ class yum::repo::centos7 (
     default => "${mirror_url}/\$releasever/centosplus/\$basearch/",
   }
 
+  if $setprio {
+    $prio_2 = {
+      failovermethod => 'priority',
+      priority       => 2,
+    }
+    $prio_3 = {
+      failovermethod => 'priority',
+      priority       => 3,
+    }
+  } else {
+    $prio_2 = {    }
+    $prio_3 = {    }
+  }
+
   yum::managed_yumrepo { 'base':
     descr          => 'CentOS-$releasever - Base',
     baseurl        => $baseurl_base,
     mirrorlist     => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os',
-    failovermethod => 'priority',
     enabled        => 1,
     gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    priority       => 2,
+    * => $prio_2
   }
 
   yum::managed_yumrepo { 'updates':
     descr          => 'CentOS-$releasever - Updates',
     baseurl        => $baseurl_updates,
     mirrorlist     => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
-    failovermethod => 'priority',
     enabled        => 1,
     gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    priority       => 2,
+    * => $prio_2
   }
 
   yum::managed_yumrepo { 'extras':
     descr          => 'CentOS-$releasever - Extras',
     baseurl        => $baseurl_extras,
     mirrorlist     => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras',
-    failovermethod => 'priority',
     enabled        => 1,
     gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    priority       => 2,
+    * => $prio_2
   }
 
   yum::managed_yumrepo { 'centosplus':
     descr          => 'CentOS-$releasever - Centosplus',
     baseurl        => $baseurl_centosplus,
     mirrorlist     => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus',
-    failovermethod => 'priority',
     enabled        => 1,
     gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    priority       => 3,
+    * => $prio_3
   }
 
 }
