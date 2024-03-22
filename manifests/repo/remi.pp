b488fe791d056af6437d0c8b5fdcd0f9f5c27680
Derk Gortemaker <derk@gortemaker.nl>
2024-03-22 15:03:59 +0100

    update remi mirror url

--- manifests/repo/remi.pp
+++ manifests/repo/remi.pp
@@ -18,10 +18,20 @@ class yum::repo::remi {
     default       => 'Enterprise Linux',
   }
 
+  if $os == 'enterprise' {
+    if $::operatingsystemmajrelease >= '8' {
+      $mirrorlist = "http://cdn.remirepo.net/${os}/\$releasever/remi/\$basearch/mirror"
+    } else {
+      $mirrorlist = "http://rpms.remirepo.net/${os}/\$releasever/remi/mirror"
+    }
+  } else {
+    $mirrorlist = "http://rpms.remirepo.net/${os}/${releasever}/remi/mirror"
+  }
+
   yum::managed_yumrepo { 'remi':
     descr      => "Remi's RPM repository for ${osname} \$releasever - \$basearch",
     # mirrorlist => "http://rpms.remirepo.net/${os}/${releasever}/remi/mirror",
-    mirrorlist => "http://cdn.remirepo.net/${os}/${releasever}/remi/\$basearch/mirror",
+    mirrorlist => $mirrorlist,
     enabled    => 1,
     gpgcheck   => 1,
     gpgkey     => 'http://rpms.remirepo.net/RPM-GPG-KEY-remi',
